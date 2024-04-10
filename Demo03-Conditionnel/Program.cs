d30781891262840639a76ef8f0c963076f335bf1
Legrain Samuel <samuel.legrain@bstorm.be>
2024-04-10 16:33:13 +0200

    Ajout Demo-03

---
+++ Demo03-Conditionnel/Program.cs
@@ -0,0 +1,37 @@
+﻿const int LIMIT_AGE = 18;
+
+Console.WriteLine("Bonjour, veuillez indiquer votre âge :");
+if(int.TryParse(Console.ReadLine(), out int age))
+{
+    if(age >= LIMIT_AGE)
+    {
+        Console.WriteLine("Veuillez indiquer votre nationalité :");
+        string nationalite = Console.ReadLine();
+        switch (nationalite.ToLower())
+        {
+            case "belge":
+                Console.WriteLine("Bienvenue à la loterie nationale belge, une fois!");
+                break;
+            case "française":
+                Console.WriteLine("Bienvenue à la loterie nationale françaiseanh");
+                break;
+            case "suisse":
+                Console.WriteLine("Bienvenueeeuhhh à la loterieeeuhhh nationaleeeuhhh suisseeeuhhh");
+                break;
+            case "canadienne":
+                Console.WriteLine("Bienvenue à la loterie nationale canadienne, tabernacle!");
+                break;
+            default:
+                Console.WriteLine("Désolé, mais vous n'êtes pas né du bon côté de la frontière...");
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Reviens dans {LIMIT_AGE - age} ans.");
+    }
+}
+else
+{
+    Console.WriteLine("Veuillez entrer un nombre correct.");
+}
