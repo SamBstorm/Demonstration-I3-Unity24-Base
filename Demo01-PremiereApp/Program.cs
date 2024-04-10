79cb53b381f6aac3a8b81a966f30e1b6177a0f5d
Legrain Samuel <samuel.legrain@bstorm.be>
2024-04-10 13:48:16 +0200

    Ajout Demo01 & Demo02

--- Demo01-PremiereApp/Program.cs
+++ Demo01-PremiereApp/Program.cs
@@ -1,15 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, les Unitys 24!");
-Console.WriteLine("Pas mal pour un premier jour de cours!");
-Console.WriteLine("Quel est votre nom ?");
-string username = Console.ReadLine();
+﻿string username;
+
+
+Console.WriteLine("Hello, les Unitys 24!\nPas mal pour un premier jour de cours!\nQuel est votre nom ?");
+username = Console.ReadLine();
 Console.Clear();
 //Console.Write("Bonjour ");
 //Console.Write(username);
 //Console.Write(" !");
 
 //Concaténation : Colle les mots ensembles
-Console.WriteLine("Bonjour " + username + " !");
+Console.WriteLine("Bonjour "+username+" !");
 
 //Interpolation : incruste les mots des variables dans le texte
 Console.WriteLine("Bonjour {0} !", username);
---
+++ Demo02-Convertion/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Veuillez introduire un nombre :");
+string userInput = Console.ReadLine();
+
+Console.WriteLine($"Vous avez introduit {userInput} de type : {userInput.GetType()}");
+
+//Convertion en Int32
+//int userNumber = Convert.ToInt32(userInput);
+//int userNumber = int.Parse(userInput);
+int userNumber;
+bool isConverted = int.TryParse(userInput, out userNumber);
+Console.WriteLine($"Convertion effectuée ? {isConverted}");
+Console.WriteLine($"Vous avez introduit {userNumber} de type : {userNumber.GetType()}");
+
+//Convertion en Double
+//double userDouble = Convert.ToDouble(userInput);
+//double userDouble = double.Parse(userInput);
+double userDouble;
+isConverted = double.TryParse(userInput, out userDouble);
+Console.WriteLine($"Convertion effectuée ? {isConverted}");
+Console.WriteLine($"Vous avez introduit {userDouble} de type : {userDouble.GetType()}");
+
+//Convertion en Boolean
+//bool userBool = Convert.ToBoolean(userInput);
+//bool userBool = bool.Parse(userInput);
+bool userBool;
+isConverted = bool.TryParse(userInput, out userBool);
+Console.WriteLine($"Convertion effectuée ? {isConverted}");
+Console.WriteLine($"Vous avez introduit {userBool} de type : {userBool.GetType()}");
