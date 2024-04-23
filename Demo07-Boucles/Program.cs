514d178b45ff0b35f83fd77db8fce3fdf5fd991c
Legrain Samuel <samuel.legrain@bstorm.be>
2024-04-23 15:55:19 +0200

    Ajout Demo07 Les boucles

---
+++ Demo07-Boucles/Program.cs
@@ -0,0 +1,32 @@
+﻿#region Exemple avec WHILE
+//Console.WriteLine("Veuillez entrer un nombre de 12 chiffres : ");
+//string bban = Console.ReadLine();                                       //Initialisation
+
+//while (bban.Length != 12 || !long.TryParse(bban, out _))                //Condition
+//{
+//    Console.WriteLine("Ceci n'est pas un BBAN valide...");
+//    Console.WriteLine("Veuillez entrer un nombre de 12 chiffres : ");
+//    bban = Console.ReadLine();                                          //Modifiée
+//}
+#endregion
+
+#region Exemple avec FOR
+//Console.WriteLine("Veuillez entrer un nombre de 12 chiffres : ");
+//for (string bban = Console.ReadLine(); bban.Length != 12 || !long.TryParse(bban, out _); bban = Console.ReadLine())
+//{
+//    Console.WriteLine("Ceci n'est pas un BBAN valide...");
+//    Console.WriteLine("Veuillez entrer un nombre de 12 chiffres : ");
+//}
+#endregion
+
+#region Exemple pour le DO ... WHILE
+
+string bban;
+do
+{
+    Console.WriteLine("Veuillez entrer un nombre de 12 chiffres : ");
+    bban = Console.ReadLine();
+} while (bban.Length != 12 || !long.TryParse(bban, out _));
+
+
+#endregion
