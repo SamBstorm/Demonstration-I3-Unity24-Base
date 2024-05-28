89256706bf94ee2a98f5f50a8a756be929beff4a
Legrain Samuel <samuel.legrain@bstorm.be>
2024-05-28 14:36:11 +0200

    Ajout Démonstration : Les tableaux de structures nullables multi-dimensionnels

--- Demo11-Structures/Program.cs
+++ Demo11-Structures/Program.cs
@@ -4,10 +4,20 @@ rgbBody.green = 240;
 rgbBody.blue = 240;
 //rgbBody.opacity = 0.5F;
 
-Console.WriteLine($"Le fond de mon site web à une couleur rgb({rgbBody.red},{rgbBody.green}, {rgbBody.blue})");
+Console.WriteLine($"Le fond de mon site web a une couleur rgb({rgbBody.red},{rgbBody.green}, {rgbBody.blue})");
 Console.WriteLine($"Le fond est opaque à {rgbBody.opacity * 100} %");
 
+RedGreenBlue rgbText = rgbBody;
+rgbText.red = (ushort)(255 - rgbText.red);
+rgbText.green = (ushort)(255 - rgbText.green);
+rgbText.blue = (ushort)(255 - rgbText.blue);
+rgbText.opacity = 1 - rgbText.opacity;
 
+Console.WriteLine($"Les textes de mon site web sont d'une couleur rgb({rgbText.red},{rgbText.green}, {rgbText.blue})");
+Console.WriteLine($"Les textes sont opaques à {rgbText.opacity * 100} %");
+
+Console.WriteLine($"Le fond de mon site web a une couleur rgb({rgbBody.red},{rgbBody.green}, {rgbBody.blue})");
+Console.WriteLine($"Le fond est opaque à {rgbBody.opacity * 100} %");
 public struct RedGreenBlue
 {
     /// <summary>
---
+++ Demo12-StructureTableau/Program.cs
@@ -0,0 +1,39 @@
+﻿Personne?[,] eleves = new Personne?[3,5];
+
+//foreach (Personne? etudiant in eleves)
+//{
+//    Console.WriteLine(etudiant);
+//}
+
+Personne sveva;
+sveva.prenom = "Sveva";
+sveva.nom = "Grippa";
+eleves[0,0] = sveva;
+Personne m_a;
+m_a.prenom = "Marie-Astrid";
+m_a.nom = "Buelens";
+eleves[0,2] = m_a;
+
+
+for(int range = 0; range < 3; range++)
+{
+    for(int place = 0; place < 5; place++)
+    {
+        Personne? etudiant = eleves[range,place];
+        if(etudiant is null)
+        {
+            Console.Write("Pas d'étudiant\t");
+        }
+        else
+        { 
+            Console.Write($"Prénom : {((Personne)etudiant).prenom} ; Nom : {((Personne)etudiant).nom}\t");
+        }
+    }
+    Console.WriteLine();
+}
+
+public struct Personne
+{
+    public string prenom;
+    public string nom;
+}
